6fae9a504ba2b25e83752cf8400be0f8e9be7637
Xieee <justinjayxie@gmail.com>
2020-05-01 15:19:55 -0700

    Created and added signup form

--- js/Notifications.js
+++ js/Notifications.js
@@ -2,20 +2,20 @@
 //For this to work npm install twilio first
 //This is for SMS notifications
 const accountSid = 'AC2b81209f000de3f1ff317ae46a0ec21c'; //This is Justin's account id
-const authToken = '70947ac19721ba57859041f7cde79b2d'; //This is Justin's authentication token
+const authToken = '7003301b1b83045df904d43f81b75495'; //This is Justin's authentication token
 
 const client = require('twilio')(accountSid, authToken);
 
 client.messages.create({
-        body: 'Alert! Your child has been misbehaving, please check on them immediately. Thank you.',
+        body: 'Alert! Your child has been misbehaving, please check on them immediately. Thank you',
         from: '+12015845237', //This is the phone number that I bought from twilio using the free trial thing
-        to: '+17789976802' //This is my number, we will change it to the nubmer that the user inputs once our firebase is working properly
-})
-    .then(message=> console.log(message.sid));
+        to: '+16048318237' //This is my number, we will change it to the nubmer that the user inputs once our firebase is working properly
+}).then(message=> console.log(message.sid));
 
 
 
 //Further testing will be done once Firebase is set up
+/*
 var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
@@ -40,4 +40,5 @@ transporter.sendMail(mailOptions, function(error, info) {
         console.log('Email sent');
     }
 });
+*/
 
---
+++ js/signup_activity.js
@@ -0,0 +1,5 @@
+function toggle() {
+    var signup = document.getElementById('signupForm');
+    signup.classList.toggle('active');
+}
+
